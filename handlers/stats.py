54767d662dfc87bf541b21ba8568652a6d3623d9
rightfold <rightfold@gmail.com>
2016-09-10 20:12:04 +0200

    Erase old events before computing stats

--- handlers/stats.py
+++ handlers/stats.py
@@ -4,27 +4,29 @@ from datetime import datetime, timedelta
 recent = timedelta(seconds=10)
 recent_events = deque()
 
-def record_event(now, event):
+def record_event(event):
+    now = datetime.now()
     recent_events.append((now, event))
 
-def erase_old_events(now):
+def erase_old_events():
+    now = datetime.now()
     while (bool(recent_events)
             and now - recent_events[0][0] > recent):
         recent_events.popleft()
 
 def events_per_second():
+    erase_old_events()
     return len(recent_events) / recent.total_seconds()
 
 def freq_per_level():
+    erase_old_events()
     freqs = defaultdict(int)
     for event in recent_events:
         freqs[event[1].level] += 1
     return freqs
 
 def on_event(event):
-    now = datetime.now()
-    record_event(now, event)
-    erase_old_events(now)
+    record_event(event)
     ε = events_per_second()
     φ = freq_per_level()
     print('events per second: {}'.format(ε))
