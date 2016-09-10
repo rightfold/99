0d9edcf50f12fd8baaae37b889caea7a288e93c2
rightfold <rightfold@gmail.com>
2016-09-10 23:20:26 +0200

    Remove stat printing from stats handler

--- handlers/stats.py
+++ handlers/stats.py
@@ -1,7 +1,2 @@
 def on_event(event):
     undocumented_stats.record_event(event)
-
-    ε = undocumented_stats.events_per_second()
-    φ = undocumented_stats.freq_per_level()
-    print('events per second: {}'.format(ε))
-    print('freq per level: {}'.format(φ))
