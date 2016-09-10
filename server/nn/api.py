244245196be96c6d5c0c08a9bc98445fd2b4db1f
rightfold <rightfold@gmail.com>
2016-09-10 23:16:13 +0200

    Publish statistics

---
+++ server/nn/api.py
@@ -0,0 +1,36 @@
+from functools import partial
+import json
+from nn.event import Level
+from werkzeug import routing
+from werkzeug.exceptions import HTTPException
+from werkzeug.wrappers import Request, Response
+
+def get_stats(stats, request):
+    ε = stats.events_per_second()
+    f = stats.freq_per_level()
+    json_stats = {
+        'eventsPerSecond': ε,
+        'debugRate':    f[Level.DEBUG],
+        'infoRate':     f[Level.INFO],
+        'noticeRate':   f[Level.NOTICE],
+        'warningRate':  f[Level.WARNING],
+        'errorRate':    f[Level.ERROR],
+        'criticalRate': f[Level.CRITICAL],
+        'alertRate':    f[Level.ALERT],
+    }
+    return Response(json.dumps(json_stats))
+
+def make_wsgi(stats):
+    routes = routing.Map([
+        routing.Rule('/stats', endpoint=partial(get_stats, stats)),
+    ])
+
+    @Request.application
+    def wsgi(request):
+        adapter = routes.bind_to_environ(request.environ)
+        try:
+            endpoint, values = adapter.match()
+            return endpoint(request, **values)
+        except HTTPException as e:
+            return e
+    return wsgi
