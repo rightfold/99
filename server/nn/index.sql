66c3b078d6e2fe67816eaffbfb21f9dc53dfc7fe
rightfold <rightfold@gmail.com>
2016-09-11 18:40:03 +0200

    Drop table logs
    The logs table is unnecessary, since the set of logs can be derived
    from the set of events using DISTINCT (which is efficient because
    an index-only scan on events__log can be used). Besides, having to
    manually create logs is a bit of a pain.

--- server/nn/index.sql
+++ server/nn/index.sql
@@ -1,21 +1,13 @@
 CREATE EXTENSION IF NOT EXISTS hstore;
 
-CREATE TABLE IF NOT EXISTS logs (
-    name char(64) NOT NULL,
-    PRIMARY KEY (name)
-);
-
 CREATE TABLE IF NOT EXISTS events (
     id          uuid            NOT NULL,
-    log         char(64)        NOT NULL,
+    log         text            NOT NULL,
     timestamp   timestamptz     NOT NULL,
     host        text            NOT NULL,
     level       int             NOT NULL,
     fields      hstore          NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (log)
-        REFERENCES logs(name)
-        ON DELETE CASCADE,
     CHECK (level BETWEEN 1 AND 7)
 );
 
