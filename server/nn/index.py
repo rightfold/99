c3ae3a9f6f7837c1abdcb091802eabb8aab6d976
rightfold <rightfold@gmail.com>
2016-09-12 21:33:48 +0200

    Add index event recording test

--- server/nn/index.py
+++ server/nn/index.py
@@ -1,6 +1,9 @@
+from datetime import datetime
 from nn.event import Event, Level
 import os.path
 import psycopg2.extras
+import subprocess
+import unittest
 from uuid import uuid4
 
 class Index:
@@ -10,25 +13,27 @@ class Index:
 
     def _setup(self):
         self.db.autocommit = True
-        psycopg2.extras.register_hstore(self.db)
         with self.db.cursor() as cur:
             sql_path = os.path.dirname(__file__) + '/index.sql'
             with open(sql_path, 'r', encoding='utf8') as sql:
                 cur.execute(sql.read())
+        psycopg2.extras.register_hstore(self.db)
 
     def record_event(self, event):
+        id = uuid4()
         with self.db.cursor() as cur:
             cur.execute('''
                 INSERT INTO events (id, log, timestamp, host, level, fields)
                 VALUES (%s, %s, %s, %s, %s, %s)
             ''', (
-                str(uuid4()),
+                str(id),
                 event.log,
                 event.timestamp,
                 event.host,
                 event.level.value,
                 event.fields,
             ))
+        return id
 
     def search_events(self, query):
         sql, args = _compile_query(query)
@@ -66,3 +71,35 @@ def _compile_query(query):
     args = []
     sql += go(query, args)
     return sql, args
+
+class IndexTest(unittest.TestCase):
+    def setUp(self):
+        subprocess.run(['dropdb', '-U', 'postgres', '--if-exists', '99_nn_index_test'])
+        subprocess.run(['createdb', '-U', 'postgres', '99_nn_index_test'])
+        self.db = psycopg2.connect('dbname=99_nn_index_test user=postgres')
+        self.index = Index(self.db)
+
+    def _event_examples(self):
+        for log in ['', 'a', 'a b', 'a/b/c', 'a\nb', 'føo', 'ömg']:
+         for host in ['', 'localhost', 'localHost', '192.168.1.23', 'exαmple.com']:
+          for level in Level:
+           for fields in [{}, {'': ''}, {'α': 'søk'}, {'bla': 'foo', 'bar': 'foo'}, {'baz': 'qux', 'foo': 'bar'}]:
+            yield Event(log, datetime.now(), host, level, fields)
+
+    def test_record_event(self):
+        for event in self._event_examples():
+            id = self.index.record_event(event)
+            with self.db.cursor() as cur:
+                cur.execute('''
+                    SELECT log, timestamp, host, level, fields
+                    FROM events
+                    WHERE id = %s
+                ''', (str(id),))
+                row = cur.fetchone()
+                self.assertIsNotNone(row)
+                log, timestamp, host, level, fields = row
+                self.assertEqual(log, event.log)
+                self.assertEqual(timestamp.replace(tzinfo=None), event.timestamp)
+                self.assertEqual(host, event.host)
+                self.assertEqual(level, event.level.value)
+                self.assertEqual(fields, event.fields)
