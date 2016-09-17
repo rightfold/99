6c09e580011c4b14d376cc18d15623c8509f18e7
rightfold <rightfold@gmail.com>
2016-09-18 00:36:00 +0200

    Add boot script

---
+++ handlers/print.go
@@ -0,0 +1,11 @@
+package handlers
+
+import "nnd/event"
+
+func StdoutHandler(event *event.Event) error {
+	return nil
+}
+
+func StderrHandler(event *event.Event) error {
+	return nil
+}
---
+++ server/src/nnd/start/start.go
@@ -0,0 +1,5 @@
+package start
+
+import "nnd/event"
+
+func Start(handler []func(*event.Event) error) {}
