bcfdb51ab42abcea320a978b5cff5cdcd71ab4ef
rightfold <rightfold@gmail.com>
2016-09-18 00:48:47 +0200

    Start funnel

--- server/src/nnd/start/start.go
+++ server/src/nnd/start/start.go
@@ -1,5 +1,28 @@
 package start
 
-import "nnd/event"
+import (
+	"net"
+	"nnd/event"
+	"nnd/funnel"
 
-func Start(handler []func(*event.Event) error) {}
+	"github.com/Sirupsen/logrus"
+)
+
+const eventsBufSize = 1024
+
+func Start(handler []func(*event.Event) error) {
+	events := make(chan *event.Event, eventsBufSize)
+	go startFunnel(events)
+	select {}
+}
+
+func startFunnel(events chan<- *event.Event) {
+	network, addr := "tcp", "0.0.0.0:1337"
+	logrus.WithField("net", network).WithField("addr", addr).Info("funnel boot")
+	funnelListener, err := net.Listen(network, addr)
+	if err != nil {
+		logrus.WithField("err", err).Fatal("funnel boot failure")
+	}
+	err = funnel.ServeAll(funnelListener, events)
+	logrus.WithField("err", err).Fatal("funnel failure")
+}
